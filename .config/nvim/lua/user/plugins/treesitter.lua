acb0f516e9d6b42c0ac86b998e5b0b31fa73628e
whoop-t <tommymallis@gmail.com>
2023-04-19 20:52:00 -0700

    Update ensure installed for treesitter

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "javascript", "typescript", "json", "yaml", "dockerfile" },
   },
 }
