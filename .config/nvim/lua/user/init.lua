f63100f5f4ebf4cee062afb9a3904c4fd57c56fd
whoop-t <tommymallis@gmail.com>
2023-04-20 20:54:37 -0700

    remove community and use own colorscheme

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "tokyonight-night",
+  colorscheme = "tokyonight",
   -- colorscheme = "astrodark",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
---
+++ .config/nvim/lua/user/plugins/tokyonight.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  opts = {
+    style = 'night',
+    transparent = true
+  }
+}
