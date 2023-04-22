55168bb676f35957d00eb58cfa575876f2c6f91a
whoop-t <tommymallis@gmail.com>
2023-04-22 00:51:36 -0700

    add harpoon back

---
+++ .config/nvim/lua/user/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+  "ThePrimeagen/harpoon",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = { "Harpoon" },
+  keys = {
+    { "<leader>a", function() require("harpoon.mark").add_file() end,        desc = "Harpoon Add file" },
+    { "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
+    { "<C-j>", function() require("harpoon.ui").nav_file(1) end, desc = "Nav file 1" },
+    { "<C-k>", function() require("harpoon.ui").nav_file(2) end, desc = "Nav file 2" },
+    { "<C-l>", function() require("harpoon.ui").nav_file(3) end, desc = "Nav file 3" },
+    { "<C-;>", function() require("harpoon.ui").nav_file(4) end, desc = "Nav file 4" },
+  },
+}
