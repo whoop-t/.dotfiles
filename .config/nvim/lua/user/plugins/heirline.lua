3c46ff8d5e171deb67e2600ca455ed08851fd751
whoop-t <tommymallis@gmail.com>
2023-04-22 13:51:03 -0700

    Add comment with custom heirline example

--- .config/nvim/lua/user/plugins/heirline.lua
+++ .config/nvim/lua/user/plugins/heirline.lua
@@ -13,6 +13,23 @@ return {
             hl = { fg = "fg", bg = "bg" },
             status.component.mode(),
             status.component.git_branch(),
+            -- Below is custom filename component that will show filename and 1 dir above
+            -- Leaving as comment, incase I want to extend/use later
+            -- {
+            --     provider = function(self)
+            --         local filepath = vim.fn.expand("%:p")
+            --         local parentdir = vim.fn.fnamemodify(filepath, ":h:t")
+            --         local filename = vim.fn.fnamemodify(filepath, ":t")
+            --         return parentdir .. '/' .. filename
+            --     end,
+            --     condition = function(self)
+            --         self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
+            --         return not status.condition.buffer_matches(
+            --             { filetype = { "aerial", "dapui_.", "neo%-tree", "NvimTree" } },
+            --             vim.api.nvim_win_get_buf(self.winid)
+            --         )
+            --     end,
+            -- },
             status.component.file_info {
                 -- Set relative path name
                 filename = { modify = ":~:." },
@@ -21,7 +38,9 @@ return {
                 condition = function(self)
                     self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
                     return not status.condition.buffer_matches(
-                        { filetype = { "aerial", "dapui_.", "neo%-tree", "NvimTree" } },
+                        {
+                            filetype = { "aerial", "dapui_.", "neo%-tree", "NvimTree" }
+                        },
                         vim.api.nvim_win_get_buf(self.winid)
                     )
                 end,
