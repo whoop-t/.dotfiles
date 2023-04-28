b9001e748ac9c6d9b4d9609747537ba40a978374
whoop-t <tommymallis@gmail.com>
2023-04-27 21:48:44 -0700

    add css lsp

--- .config/nvim/lua/user/plugins/mason.lua
+++ .config/nvim/lua/user/plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "tsserver", "jsonls", "bashls", "html", "sqlls" },
+      ensure_installed = { "lua_ls", "tsserver", "jsonls", "bashls", "html", "cssls", "sqlls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
