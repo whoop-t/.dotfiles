402565b3bd643c4b4687530bb812b9bd4f6430c3
whoop-t <tommymallis@gmail.com>
2023-05-10 19:27:28 -0700

    change vim to lua for scrollback

---
+++ .config/kitty/scrollback-pager/nvim/init.lua
@@ -0,0 +1,38 @@
+-- This is a minimal config just for kitty scrollback pager
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.virtualedit = "all"
+vim.opt.scrollback = 100000
+vim.opt.termguicolors = true
+vim.opt.laststatus = 0
+vim.opt.background = "dark"
+vim.opt.ignorecase = true
+vim.opt.scrolloff = 8
+
+vim.cmd("hi LineNr guifg=#3b4261")
+
+vim.api.nvim_set_keymap("n", "q", ":qa!<CR>", { silent = true })
+
+-- Short highlight on yanked text
+vim.cmd([[
+  augroup highlight_yank
+    autocmd!
+    autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 40})
+  augroup END
+]])
+
+vim.cmd([[
+  augroup start_at_bottom
+    autocmd!
+    autocmd VimEnter * normal G
+  augroup END
+]])
+
+vim.cmd([[
+  augroup prevent_insert
+    autocmd!
+    autocmd TermEnter * stopinsert
+  augroup END
+]])
