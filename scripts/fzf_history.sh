f24791c6e84c5b9e7d8a0ef2d4f6f80c2302a029
whoop-t <tommymallis@gmail.com>
2023-05-17 23:08:33 -0700

    update history script to use built in "history" and copy it on select

--- scripts/fzf_history.sh
+++ scripts/fzf_history.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-selected=$(cat ~/.zsh_history | fzf)
+os=$(uname)
 
-if [ -n "$selected" ]; then
-  echo "$selected"
+if [[ "$os" == "Darwin" ]]; then
+  selected=$(history | fzf)
+  if [ -n "$selected" ]; then
+    echo "$selected" | awk '{$1=""; print $0}' | tr -d '\n' | pbcopy
+    echo "Command copied to macOS clipboard."
+  fi
+elif [[ "$os" == "Linux" ]]; then
+  selected=$(history | fzf)
+  if [ -n "$selected" ]; then
+    echo "$selected" | awk '{$1=""; print $0}' | tr -d '\n' | xclip -selection clipboard
+    echo "Command copied to Linux clipboard."
+  fi
+else
+  echo "Unsupported operating system."
 fi
 
