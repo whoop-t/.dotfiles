a7f4e3501182556b62bda242e4373460461f2a96
whoop-t <tommymallis@gmail.com>
2023-11-20 15:24:04 -0800

    chore: leap updates

--- .config/nvim/lua/user/plugins/leap.lua
+++ .config/nvim/lua/user/plugins/leap.lua
@@ -8,24 +8,9 @@ return {
       end
       return ret
     end,
-    opts = {
-      labeled_modes = "nx",
-      multiline = false
-    },
+    opts = { labeled_modes = "nx" },
     dependencies = {
       "ggandor/leap.nvim",
-      keys = {
-        { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-        { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-        { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-      },
-      config = function(_, opts)
-        local leap = require "leap"
-        for k, v in pairs(opts) do
-          leap.opts[k] = v
-        end
-        leap.add_default_mappings(true)
-      end,
       dependencies = {
         "tpope/vim-repeat",
       },
