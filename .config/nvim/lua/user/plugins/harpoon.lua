359eb0334c9106c404fedd7b00336c2b64f36a24
whoop-t <tommymallis@gmail.com>
2023-12-05 20:01:51 -0800

    fix some config issues causing bugs

--- .config/nvim/lua/user/plugins/harpoon.lua
+++ .config/nvim/lua/user/plugins/harpoon.lua
@@ -1,21 +1,21 @@
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  dependencies = {
-    'nvim-lua/plenary.nvim'
-  },
+  "ThePrimeagen/harpoon",
+  lazy = false,
+  branch = "harpoon2",
   config = function()
-    local harpoon = require("harpoon")
-    -- REQUIRED
-    harpoon:setup()
-    -- REQUIRED
+    local harpoon = require "harpoon"
+    harpoon:setup {
+      settings = {
+        ui_width_ratio = 0.4,
+      },
+    }
+
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
   end,
-  keys = {
-    { "<leader>a", function() require("harpoon"):list():append() end,                                  desc = "Harpoon Add file" },
-    { "<C-e>",     function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,  desc = "Toggle quick menu" },
-    { "<C-j>",     function() require("harpoon"):list():select(1) end,                                 desc = "Nav file 1" },
-    { "<C-k>",     function() require("harpoon"):list():select(2) end,                                 desc = "Nav file 2" },
-    { "<C-l>",     function() require("harpoon"):list():select(3) end,                                 desc = "Nav file 3" },
-    { "<C-;>",     function() require("harpoon"):list():select(4) end,                                 desc = "Nav file 4" },
-  },
+  dependencies = { "nvim-lua/plenary.nvim" },
 }
