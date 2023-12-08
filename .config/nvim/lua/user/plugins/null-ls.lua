f098b7386017bae2a8a255e44c0f33bbee222297
whoop-t <tommymallis@gmail.com>
2023-12-08 10:01:10 -0800

    conditions for prettier and eslint formatters

--- .config/nvim/lua/user/plugins/null-ls.lua
+++ .config/nvim/lua/user/plugins/null-ls.lua
@@ -10,7 +10,20 @@ return {
     config.sources = {
       -- Set a formatter
       null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettierd,
+      -- Check if .prettierrc, if true, use prettierd, else use eslint
+      -- This is for working on multiple projects where one uses prettier and the other just has eslint
+      null_ls.builtins.formatting.prettierd.with {
+        condition = function(utils)
+          local file_check_result = utils.has_file(".prettierrc", ".prettierrc.js", ".prettierrc.json", ".prettierrc.yaml")
+          return file_check_result and nil
+        end,
+      },
+      null_ls.builtins.formatting.eslint.with {
+        condition = function(utils)
+          local file_check_result = utils.has_file(".prettierrc", ".prettierrc.js", ".prettierrc.json", ".prettierrc.yaml")
+          return not file_check_result
+        end,
+      },
       -- Set a linter
       null_ls.builtins.diagnostics.eslint,
       -- Set code actions
