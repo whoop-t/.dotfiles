d98aeb16413b9ca7706ea610567a5340676e6298
whoop-t <tommymallis@gmail.com>
2023-12-07 16:33:51 -0800

    fix extra open bracket

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -55,7 +55,6 @@ return {
         -- filter = function(client) -- fully override the default formatting function
         --   return true
         -- end
-      },
       timeout_ms = 5000, -- default format timeout
     },
     -- enable servers that you already have installed without mason
