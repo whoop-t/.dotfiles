f67116ae2775fa42aea918a896deaefe5325ab3e
whoop-t <tommymallis@gmail.com>
2024-02-16 09:49:21 -0800

    update fzf search script

--- scripts/fzf_dev.sh
+++ scripts/fzf_dev.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
+# Specify additional directories to search for
+dirs=(
+  "$HOME/.dotfiles"
+  "$HOME/dev"
+  "$HOME/Documents"
+  # Add more directories as needed
+)
 
-dir=$(find $HOME/dev -mindepth 1 -maxdepth 2 -type d -not -path "*/\.*" -not -path "*/\.git/*" -not -path "*/\node_modules/*" -print 2> /dev/null | fzf)
+dir=$(find "${dirs[@]}" -maxdepth 4 -type d -not -path "*/\.git/*" -not -path "*/\node_modules/*" -print 2> /dev/null | fzf)
 
 if [ -n "$dir" ]; then
   cd "$dir" || echo "Failed to change directory to: $dir"
 else
   echo "No directory selected"
 fi
-
