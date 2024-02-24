83c90ebebe1203bf1bf063fd2ea35005aa75af20
whoop-t <tommymallis@gmail.com>
2024-02-24 09:07:04 -0800

    update heirline to exclude telescope, lazy, help. Add back lsp info

--- .config/nvim/lua/user/plugins/heirline.lua
+++ .config/nvim/lua/user/plugins/heirline.lua
@@ -1,40 +1,35 @@
 return {
-    "rebelot/heirline.nvim",
-    opts = function(_, opts)
-        local status = require "astronvim.utils.status"
-        -- local harpoon_components = require "harpoon.heirline"
+  "rebelot/heirline.nvim",
+  opts = function(_, opts)
+    local status = require "astronvim.utils.status"
 
-        opts.winbar = nil
-        opts.tabline = nil
-        opts.statusline = {
-            -- statusline
-            hl = { fg = "fg", bg = "bg" },
-            status.component.mode(),
-            status.component.git_branch(),
-            -- harpoon_components.index, // moved to harpoon2, need to revisit
-            status.component.file_info {
-                -- Set relative path name
-                filename = { modify = ":~:." },
-                -- Only show filename when its file, not neotree, telescope, etc
-                -- Not sure how to disable for lazygit
-                condition = function(self)
-                    self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
-                    return not status.condition.buffer_matches(
-                        {
-                            filetype = { "aerial", "dapui_.", "neo%-tree", "NvimTree" }
-                        },
-                        vim.api.nvim_win_get_buf(self.winid)
-                    )
-                end,
-            },
-            status.component.diagnostics(),
-            status.component.fill(),
-            -- lsp causes issue on mac with tokyonight(https://discord.com/channels/939594913560031363/1100223017017163826)
-            -- status.component.lsp(),
-            status.component.treesitter(),
-            status.component.nav(),
-        }
+    opts.winbar = nil
+    opts.tabline = nil
+    opts.statusline = {
+      -- statusline
+      hl = { fg = "fg", bg = "bg" },
+      status.component.mode(),
+      status.component.git_branch(),
+      -- harpoon_components.index, // moved to harpoon2, need to revisit
+      status.component.file_info {
+        -- Set relative path name
+        filename = { modify = ":~:." },
+        -- Only show filename when its file, not neotree, telescope, etc
+        condition = function()
+          return not status.condition.buffer_matches {
+            buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
+            filetype = { "aerial", "dapui_.", "neo%-tree", "NvimTree" },
+          }
+        end,
+      },
+      status.component.diagnostics(),
+      status.component.fill(),
+      -- lsp causes issue on mac with tokyonight(https://discord.com/channels/939594913560031363/1100223017017163826)
+      status.component.lsp(),
+      status.component.treesitter(),
+      status.component.nav(),
+    }
 
-        return opts
-    end,
+    return opts
+  end,
 }
