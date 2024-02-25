01b8ec70617cff9486338ddebc431ad9e504e3dc
whoop-t <tommymallis@gmail.com>
2024-02-25 11:55:04 -0800

    fix(personal): surround event

--- .config/personal_nvim/lua/plugins/nvim-surround.lua
+++ .config/personal_nvim/lua/plugins/nvim-surround.lua
@@ -1,6 +1,6 @@
 return {
   "kylechui/nvim-surround",
-  event = "User AstroFile",
+  event = "BufEnter",
   config = function()
     require("nvim-surround").setup {
       -- Configuration here, or leave empty to use defaults
