e88b56192011e4a2b94ebbb883d5fd397b6c42db
whoop-t <tommymallis@gmail.com>
2024-02-25 01:09:10 -0800

    update astrocore mappings to new naming to fix bugs

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -46,13 +46,13 @@ return {
         ["<C-k>"] = false,
         ["<C-l>"] = false,
         ["<C-;>"] = false,
-        ["<leader>o"] = false,
-        ["<leader>p"] = { '"_dP', desc = "blackhole delete and paste" },
-        ["<leader>h"] = { "<cmd>nohlsearch<cr>", desc = "remove search highlight" },
+        ["<Leader>o"] = false,
+        ["<Leader>p"] = { '"_dP', desc = "blackhole delete and paste" },
+        ["<Leader>h"] = { "<cmd>nohlsearch<cr>", desc = "remove search highlight" },
         -- ["<leader>e"] = { "<cmd>:Neotree toggle current reveal_force_cwd<cr>", desc = "remove search highlight" },
         -- Below toggles between buffer and neotree buffer
         -- BUT it will not toggle from neotree if no other buffers open
-        ["<leader>e"] = {
+        ["<Leader>e"] = {
           function()
             local bufs = vim.fn.getbufinfo { buflisted = true }
             if vim.bo.filetype == "neo-tree" and not bufs[1] then
@@ -76,7 +76,7 @@ return {
         --   desc = "Previous buffer",
         -- },
         -- Dont allowing closing of last buffer
-        ["<leader>c"] = {
+        ["<Leader>c"] = {
           function()
             local bufs = vim.fn.getbufinfo { buflisted = true }
             if not bufs[2] then
@@ -92,10 +92,10 @@ return {
           desc = "Close buffer",
         },
         -- GitBlame mappings
-        ["<leader>gu"] = { "<CMD>GitBlameOpenCommitURL<CR>", desc = "Open Blame Url" },
+        ["<Leader>gu"] = { "<CMD>GitBlameOpenCommitURL<CR>", desc = "Open Blame Url" },
 
         -- Map lazydocker to td(requires lazydocker to be installed/in bin)
-        ["<leader>td"] = {
+        ["<Leader>td"] = {
           function()
             if vim.fn.executable "lazydocker" == 1 and require("astronvim.utils").is_available "toggleterm.nvim" then
               require("astronvim.utils").toggle_term_cmd "lazydocker"
@@ -106,7 +106,7 @@ return {
       },
       t = {},
       v = {
-        ["<leader>p"] = { '"_dP', desc = "blackhole delete and paste" },
+        ["<Leader>p"] = { '"_dP', desc = "blackhole delete and paste" },
         -- Visual mode line move
         -- ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
         -- ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line up"},
