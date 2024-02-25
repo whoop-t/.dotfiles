1a8e30070c15bff728f44e03ff430f1f57b407ba
whoop-t <tommymallis@gmail.com>
2024-02-25 11:53:03 -0800

    alter lualine

--- .config/personal_nvim/init.lua
+++ .config/personal_nvim/init.lua
@@ -19,3 +19,18 @@ require "autocmds"
 -- Load plugins, lazy will do this automagically if string is in lua/{your dir here}. I use 'plugins'
 require("lazy").setup "plugins"
 vim.cmd.colorscheme "tokyonight"
+
+-- Highlights(is there a better place to put this)
+vim.cmd [[
+  highlight lualine_b_visual guibg=NONE
+  highlight lualine_b_normal guibg=NONE
+  highlight lualine_b_insert guibg=NONE
+  highlight lualine_a_inactive guibg=NONE
+  highlight lualine_b_inactive guibg=NONE
+  highlight lualine_b_replace guibg=NONE
+  highlight lualine_b_command guibg=NONE
+  highlight lualine_c_inactive guibg=NONE
+  highlight lualine_c_normal guibg=NONE
+  highlight lualine_a_terminal guibg=NONE
+  highlight lualine_b_terminal guibg=NONE
+]]
--- .config/personal_nvim/lua/plugins/lualine.lua
+++ .config/personal_nvim/lua/plugins/lualine.lua
@@ -2,31 +2,44 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      options = {
-        theme = "tokyonight",
-        component_separators = "|",
-      },
-      sections = {
-        lualine_a = {
-          { "mode", right_padding = 2 },
+    opts = function()
+      return {
+        options = {
+          -- Its not using this theme current, idk why yet. Loading issue?
+          theme = "tokyonight",
+          component_separators = "|",
+          disabled_filetypes = {
+            statusline = {
+              "dashboard",
+              "aerial",
+              "dapui_.",
+              "neo%-tree",
+              "NvimTree",
+            },
+          },
+          section_separators = { left = "", right = "" },
         },
-        lualine_b = { "filename", "branch" },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = { "filetype", "progress" },
-        lualine_z = {
-          { "location", left_padding = 2 },
+        sections = {
+          lualine_a = {},
+          lualine_b = {
+            "branch",
+            -- { "filetype", icon_only = true, separator = "", padding = { right = 0 } }, --disabling cause idk how to make bg transparent of icon
+            "filename",
+          },
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
         },
-      },
-      inactive_sections = {
-        lualine_a = { "filename" },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { "location" },
-      },
-    },
+        inactive_sections = {
+          lualine_a = { "filename" },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+      }
+    end,
   },
 }
