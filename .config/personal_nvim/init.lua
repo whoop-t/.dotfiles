947b6e1dcb651104db13862c76b47f486fad4b64
whoop-t <tommymallis@gmail.com>
2024-02-25 10:54:31 -0800

    updates to personal

--- .config/personal_nvim/init.lua
+++ .config/personal_nvim/init.lua
@@ -1,21 +1,21 @@
 -- Set up lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load other lua files
-require("options")
-require("mappings")
-require("autocmds")
+require "options"
+require "mappings"
+require "autocmds"
 -- Load plugins, lazy will do this automagically if string is in lua/{your dir here}. I use 'plugins'
-require("lazy").setup("plugins")
-vim.cmd.colorscheme("tokyonight")
+require("lazy").setup "plugins"
+vim.cmd.colorscheme "tokyonight"
--- .config/personal_nvim/lua/autocmds.lua
+++ .config/personal_nvim/lua/autocmds.lua
@@ -52,9 +52,28 @@ autocmd("BufWinEnter", {
     end
   end,
 })
+
+-- Quick highlight visual on yank
 autocmd("TextYankPost", {
   desc = "Highlight yanked text",
   group = augroup("highlightyank", { clear = true }),
   pattern = "*",
   callback = function() vim.highlight.on_yank() end,
+})
+
+-- Spell and wrap on certain files
+vim.api.nvim_create_autocmd("BufRead", {
+  desc = "Turn spell and wrap on for certain files",
+  pattern = { "*.txt", "*.md" },
+  group = augroup("spellcheck", { clear = true }),
+  callback = function()
+    local current_file = vim.fn.expand "%:p"
+
+    -- Exclude files in nvim/runtime/doc with a .txt extension
+    if current_file:match "/nvim/runtime/doc/.*%.txt$" then return end
+
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+    require "notify" "spell and wrap on"
+  end,
 })
