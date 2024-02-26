0db304e3a2e8f49f5f1f1f60fa43188e734be823
whoop-t <tommymallis@gmail.com>
2024-02-25 19:00:57 -0800

    feat(personal): telescope ui changes

--- .config/personal_nvim/lua/plugins/telescope.lua
+++ .config/personal_nvim/lua/plugins/telescope.lua
@@ -19,7 +19,36 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
+    },
+    cmd = "Telescope",
+    opts = function()
+      local actions = require "telescope.actions"
+      return {
+        defaults = {
+          git_worktrees = vim.g.git_worktrees,
+          path_display = { "truncate" },
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = { prompt_position = "top", preview_width = 0.55 },
+            vertical = { mirror = false },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
+          mappings = {
+            i = {
+              ["<C-n>"] = actions.cycle_history_next,
+              ["<C-p>"] = actions.cycle_history_prev,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
+            n = { q = actions.close },
+          },
+        },
+      }
+    end,
     keys = function()
       local builtin = require "telescope.builtin"
       return {
