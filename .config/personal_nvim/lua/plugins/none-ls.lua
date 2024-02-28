670d7b20a16a27c3a6fa96173359f880a9bc4e4b
whoop-t <tommymallis@gmail.com>
2024-02-27 19:44:32 -0800

    feat(personal): remove eslint_d

--- .config/personal_nvim/lua/plugins/none-ls.lua
+++ .config/personal_nvim/lua/plugins/none-ls.lua
@@ -1,18 +1,20 @@
 return {
   "nvimtools/none-ls.nvim",
   config = function()
-    local null_ls = require("null-ls")
-
-    null_ls.setup({
+    local null_ls = require "null-ls"
+    null_ls.setup {
       sources = {
         -- Spell
         null_ls.builtins.completion.spell,
         -- Lua
         null_ls.builtins.formatting.stylua,
         -- JS
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.formatting.prettierd.with {
+          condition = function(utils)
+            return utils.root_has_file(".prettierrc", ".prettierrc.js", ".prettierrc.json", ".prettierrc.yaml")
+          end,
+        },
       },
-    })
+    }
   end,
 }
