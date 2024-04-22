0f9b9311cf43d99c62c8df124d81aaf04763f725
whoop-t <tommymallis@gmail.com>
2024-04-21 19:00:59 -0700

    fix(scripts): make history script better

--- scripts/fzf_history.sh
+++ scripts/fzf_history.sh
@@ -3,13 +3,13 @@
 os=$(uname)
 
 if [[ "$os" == "Darwin" ]]; then
-  selected=$(history | tail -r | fzf)
+  selected=$(history 1 | sort -rn | fzf --no-sort)
   if [ -n "$selected" ]; then
     echo "$selected" | awk '{$1=""; print $0}' | tr -d '\n' | pbcopy
     echo "Command copied to macOS clipboard."
   fi
 elif [[ "$os" == "Linux" ]]; then
-  selected=$(history | tac | fzf)
+  selected=$(history 1 | sort -rn | fzf --no-sort)
   if [ -n "$selected" ]; then
     echo "$selected" | awk '{$1=""; print $0}' | tr -d '\n' | xclip -selection clipboard
     echo "Command copied to Linux clipboard."
