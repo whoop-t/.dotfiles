f34cd0dde1dde915f0245f5c3030253e34ebf4c5
whoop-t <tommymallis@gmail.com>
2024-05-04 10:44:28 -0700

    fix(cloak): pattern match local*

--- .config/nvim/lua/plugins/cloak.lua
+++ .config/nvim/lua/plugins/cloak.lua
@@ -16,7 +16,7 @@ return {
         {
           -- Match any file starting with '.env'.
           -- This can be a table to match multiple file patterns.
-          file_pattern = { ".env*", ".npmrc*", "local.cjs*", "credentials*" },
+          file_pattern = { ".env*", ".npmrc*", "local*.cjs", "credentials*" },
           -- Match an equals sign and any character after it.
           -- This can also be a table of patterns to cloak,
           -- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
