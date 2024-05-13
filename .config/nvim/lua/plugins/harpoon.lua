3208a9b3f74037d9246b3e955d25065782556421
whoop-t <tommymallis@gmail.com>
2024-05-13 15:19:52 -0700

    fix(harpoon): append deprecated, move to using add

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -28,7 +28,7 @@ return {
       },
     }
 
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set(
       "n",
       "<C-e>",
