370dc53f4449f41978eebe2751cd5d4fd3abe363
whoop-t <tommymallis@gmail.com>
2024-07-03 20:02:22 -0700

    feat(astro): update telescope to include more files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,13 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      pickers = {
-        find_files = {
-          follow = true,
-        },
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    pickers = {
+      find_files = {
+        follow = true,
+        hidden = true,
+        file_ignore_patterns = { ".git/*", "node_modules/*", ".vscode/*" },
       },
     },
-  }  
+  },
+}
