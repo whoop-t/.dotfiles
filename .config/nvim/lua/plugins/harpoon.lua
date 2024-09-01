f67ecabf085a5cc289023e946a6e7640f73c7b20
whoop-t <tommymallis@gmail.com>
2024-09-01 09:17:10 -0700

    chore(astro): clean up old harpoon comments

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,21 +1,3 @@
--- LEGACY HARPOON, broken on nvim 0.9.4+ due to api change
--- return {
---   "whoop-t/harpoon-light",
---   dependencies = {
---     "nvim-lua/plenary.nvim",
---     "nvim-telescope/telescope.nvim",
---   },
---   cmd = { "Harpoon" },
---   keys = {
---     { "<leader>a", function() require("harpoon.mark").add_file() end,        desc = "Harpoon Add file" },
---     { "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
---     { "<C-j>", function() require("harpoon.ui").nav_file(1) end, desc = "Nav file 1" },
---     { "<C-k>", function() require("harpoon.ui").nav_file(2) end, desc = "Nav file 2" },
---     { "<C-l>", function() require("harpoon.ui").nav_file(3) end, desc = "Nav file 3" },
---     { "<C-;>", function() require("harpoon.ui").nav_file(4) end, desc = "Nav file 4" },
---   },
--- }
-
 return {
   "ThePrimeagen/harpoon",
   lazy = false,
