852e08f6c5c8bc73c737738b0c8500f92b89e604
whoop-t <tommymallis@gmail.com>
2024-09-08 15:32:18 -0700

    chore(astro): remove meaningless key binding

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -11,7 +11,7 @@ return {
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
+      autopairs = true, -- enable autopairs at start, I have the plugin disabled in core.lua
       cmp = true, -- enable completion at start
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
@@ -46,7 +46,6 @@ return {
         ["<Leader>o"] = false,
         ["<Leader>c"] = false,
         ["<Leader>n"] = false,
-        ["<Leader>p"] = { '"_dP', desc = "blackhole delete and paste" },
         ["<Leader>h"] = { "<cmd>nohlsearch<cr>", desc = "remove search highlight" },
         ["<Leader>cf"] = {
           '<cmd>let @+ = fnamemodify(expand("%:h:h"), ":t") . "/" . expand("%:t")<cr>',
