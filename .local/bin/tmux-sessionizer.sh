b7cc2c357548879a0e5d8f9a0667f0ae8ed6f899
whoop-t <tommymallis@gmail.com>
2024-11-17 21:50:57 -0800

    chore: add comments to tmux-sessionizer

--- .local/bin/tmux-sessionizer.sh
+++ .local/bin/tmux-sessionizer.sh
@@ -22,15 +22,18 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
+## If we arent in tmux and no tmux sessions are running
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
     # Start a new TMUX session and attach
     tmux new-session -s $selected_name -c $selected
     exit 0
 fi
 
+## Create detached tmux, its less error prone to create then switch
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
     # Create a new detached session
     tmux new-session -ds $selected_name -c $selected
 fi
 
+## Switch the the selected session, if it was created or not
 tmux switch-client -t $selected_name
