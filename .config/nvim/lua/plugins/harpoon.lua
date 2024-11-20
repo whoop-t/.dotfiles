92898dbe8cccaae5eda0abb8fa7e2bdd57e081d1
whoop-t <tommymallis@gmail.com>
2024-11-20 08:32:51 -0800

    revert: harpoon back to using ctrl

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -30,10 +30,10 @@ return {
         })
       end
     )
-    vim.keymap.set("n", "<M-m>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<M-,>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<M-.>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<M-/>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-m>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-,>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-.>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-/>", function() harpoon:list():select(4) end)
   end,
   dependencies = { "nvim-lua/plenary.nvim" },
 }
