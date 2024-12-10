a15e22b0f4fdccfee2da72b300357397a9509fc1
whoop-t <tommymallis@gmail.com>
2024-12-09 18:18:00 -0800

    feat!: redo personal config lsp

--- .config/personal_nvim/lua/mappings.lua
+++ .config/personal_nvim/lua/mappings.lua
@@ -18,3 +18,14 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>")
 -- hover open issues in file
 vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float)
+-- snippet jumping
+vim.keymap.set({ "i", "s" }, "<C-l>", function()
+  if require("luasnip").expand_or_jumpable() then
+    require("luasnip").expand_or_jump()
+  end
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-h>", function()
+  if require("luasnip").jumpable(-1) then
+    require("luasnip").jump(-1)
+  end
+end, { silent = true })
--- .config/personal_nvim/lua/plugins/lsp.lua
+++ .config/personal_nvim/lua/plugins/lsp.lua
@@ -1,101 +1,173 @@
 return {
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v3.x",
-    config = function()
-      -- should this go here?
-      local lsp_zero = require "lsp-zero"
-
-      lsp_zero.on_attach(function(_, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>lh", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format { async = true } end, opts)
-        lsp_zero.default_keymaps { buffer = bufnr } -- this will add lsp-zero defaults, but not override ones above
-      end)
-    end,
+    "williamboman/mason.nvim",
+    opts = {},
   },
-  { "williamboman/mason.nvim", opts = {} },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      local lsp_zero = require "lsp-zero"
-      lsp_zero.extend_lspconfig()
-      require("mason-lspconfig").setup {
-        automatic_installation = true,
-        ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "jsonls",
-          "bashls",
-          "html",
-          "cssls",
-          "sqlls",
-          "gopls",
-          "yamlls",
-          "pyright",
-          "emmet_language_server",
-        },
-        handlers = {
-          lsp_zero.default_setup,
-        },
-      }
-    end,
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "ts_ls",
+        "jsonls",
+        "bashls",
+        "html",
+        "cssls",
+        "sqlls",
+        "gopls",
+        "yamlls",
+        "pyright",
+        "emmet_language_server",
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    -- REQUIRED for snippets/completions to show in cmp
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-    },
     config = function()
-      local cmp = require "cmp"
-      local cmp_action = require("lsp-zero").cmp_action()
-      cmp.setup {
-        sources = {
-          -- priority will make certain completions show first, we want lsp and luasnips to show first
-          { name = "luasnip", priority = 1000 },
-          { name = "nvim_lsp", priority = 750 },
-          { name = "buffer", priority = 500 },
-          { name = "path", priority = 250 },
-        },
-        mapping = cmp.mapping.preset.insert {
-          -- `Enter` key to confirm completion
-          ["<CR>"] = cmp.mapping.confirm { select = false },
+      local lspconfig = require "lspconfig"
 
-          -- Ctrl+Space to trigger completion menu
-          ["<C-Space>"] = cmp.mapping.complete(),
-          -- Navigate between snippet placeholder
-          ["<Tab>"] = cmp_action.luasnip_jump_forward(),
-          ["<S-Tab>"] = cmp_action.luasnip_jump_backward(),
+      -- lsp setups
+      lspconfig.lua_ls.setup {}
+      lspconfig.ts_ls.setup {}
 
-          -- Scroll up and down in the completion documentation
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-      }
+      -- key bindings
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("LspKeybindings", { clear = true }),
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>lh", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format { async = true } end, opts)
+        end,
+      })
     end,
   },
   {
-    "L3MON4D3/LuaSnip",
-    -- friendly-snippets needs to be dep of luasnip, else could cause issues
-    dependencies = { "rafamadriz/friendly-snippets" },
-    config = function() require("luasnip.loaders.from_vscode").lazy_load() end,
+    "jay-babu/mason-null-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    opts = {
+      ensure_installed = { "prettierd", "stylua", "biome" },
+      automatic_installation = false,
+      handlers = {
+        prettierd = function(source_name, methods)
+          -- Conditional to only use prettier when a .pretterrc is in root
+          local null_ls = require "null-ls"
+          null_ls.register(null_ls.builtins.formatting.prettierd.with {
+            condition = function(utils)
+              return utils.root_has_file(
+                ".prettierrc",
+                ".prettierrc.js",
+                ".prettierrc.cjs",
+                ".prettierrc.json",
+                ".prettierrc.yaml",
+                ".prettierrc.yml",
+                ".prettierrc.toml",
+                "prettier.config.cjs"
+              )
+            end,
+          })
+        end,
+        eslint = function(source_name, methods)
+          -- Conditional to only use eslint when a .eslint is in root
+          local null_ls = require "null-ls"
+          null_ls.register(require("none-ls.formatting.eslint").with {
+            condition = function(utils)
+              return utils.root_has_file(
+                ".eslintrc",
+                ".eslintrc.json",
+                ".eslintrc.js",
+                ".eslintrc.yml",
+                ".eslintrc.yaml"
+              )
+            end,
+          })
+        end,
+        biome = function(source_name, methods)
+          -- Conditional to only use biome when a biome.json is in root
+          local null_ls = require "null-ls"
+          null_ls.register(null_ls.builtins.formatting.biome.with {
+            condition = function(utils) return utils.root_has_file "biome.json" end,
+          })
+        end,
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require "null-ls"
+      null_ls.setup {
+        sources = {
+          -- Lua
+          null_ls.builtins.formatting.stylua,
+          -- JS
+          null_ls.builtins.formatting.prettierd.with {
+            condition = function(utils)
+              return utils.root_has_file(
+                ".prettierrc",
+                ".prettierrc.js",
+                ".prettierrc.cjs",
+                ".prettierrc.json",
+                ".prettierrc.yaml",
+                ".prettierrc.yml",
+                ".prettierrc.toml",
+                "prettier.config.cjs"
+              )
+            end,
+          },
+          null_ls.builtins.formatting.biome.with {
+            condition = function(utils) return utils.root_has_file "biome.json" end,
+          },
+
+          -- eslint
+          require("none-ls.diagnostics.eslint").with {
+            condition = function(utils)
+              return utils.root_has_file(
+                ".eslintrc",
+                ".eslintrc.json",
+                ".eslintrc.js",
+                ".eslintrc.yml",
+                ".eslintrc.yaml"
+              )
+            end,
+          },
+
+          require("none-ls.formatting.eslint").with {
+            condition = function(utils)
+              return utils.root_has_file(
+                ".eslintrc",
+                ".eslintrc.json",
+                ".eslintrc.js",
+                ".eslintrc.yml",
+                ".eslintrc.yaml"
+              )
+            end,
+          },
+
+          require("none-ls.code_actions.eslint").with {
+            condition = function(utils)
+              return utils.root_has_file(
+                ".eslintrc",
+                ".eslintrc.json",
+                ".eslintrc.js",
+                ".eslintrc.yml",
+                ".eslintrc.yaml"
+              )
+            end,
+          },
+        },
+      }
+    end,
   },
 }
