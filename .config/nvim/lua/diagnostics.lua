bb0100fdf476163c231980b0b4b2844eadc6ae83
whoop-t <tommymallis@gmail.com>
2024-12-09 23:30:43 -0800

    fix: diag prefix

--- .config/nvim/lua/diagnostics.lua
+++ .config/nvim/lua/diagnostics.lua
@@ -14,4 +14,22 @@ for type, icon in pairs(M.signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
+-- Show diagnostics with lsp that outputted it
+-- This opens a float with diag info
+vim.keymap.set("n", "<leader>ld", function()
+  vim.diagnostic.open_float(nil, {
+    border = "rounded",
+    -- Customize how each diagnostic is formatted
+    format = function(diagnostic)
+      if diagnostic.source then
+        return string.format("[%s] %s", diagnostic.source, diagnostic.message)
+      else
+        return diagnostic.message
+      end
+    end,
+    prefix = "", -- Remove prefix aka numbered list
+    header = "", -- Remove the title
+  })
+end, { desc = "Show diagnostics with source" })
+
 return M
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -38,10 +38,6 @@ return {
       vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
         border = "rounded",
       })
-      -- Set border for diag help <leader>ld
-      vim.diagnostic.config {
-        float = { border = "rounded" },
-      }
 
       -- Setup up all language servers that are installed
       for _, value in ipairs(ensure_installed) do
@@ -69,22 +65,6 @@ return {
           vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
           -- using conform now
           -- vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format { async = true } end, opts)
-
-          -- Show diagnostics with lsp that outputted it
-          vim.keymap.set("n", "<leader>ld", function()
-            vim.diagnostic.open_float(nil, {
-              border = "rounded",
-              -- Customize how each diagnostic is formatted
-              format = function(diagnostic)
-                if diagnostic.source then
-                  return string.format("[%s] %s", diagnostic.source, diagnostic.message)
-                else
-                  return diagnostic.message
-                end
-              end,
-              header = "", -- Remove the title
-            })
-          end, { desc = "Show diagnostics with source" })
           --
         end,
       })
