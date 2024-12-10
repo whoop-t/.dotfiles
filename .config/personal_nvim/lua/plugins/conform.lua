7f06426d67da2d3132da59ffe5ea59e13cd361d8
whoop-t <tommymallis@gmail.com>
2024-12-09 21:39:39 -0800

    feat!: remove none-ls and add confrom in personal config

---
+++ .config/personal_nvim/lua/plugins/conform.lua
@@ -0,0 +1,90 @@
+local eslint_configs = {
+  ".eslintrc",
+  ".eslintrc.json",
+  ".eslintrc.js",
+  ".eslintrc.yml",
+  ".eslintrc.yaml",
+  "eslint.config.js",
+  "eslint.config.mjs",
+  "eslint.config.cjs",
+  "eslint.config.ts",
+}
+
+local prettier_configs = {
+  ".prettierrc",
+  ".prettierrc.js",
+  ".prettierrc.cjs",
+  ".prettierrc.json",
+  ".prettierrc.yaml",
+  ".prettierrc.yml",
+  ".prettierrc.toml",
+  "prettier.config.cjs",
+}
+
+local biome_configs = { "biome.json" }
+
+local function formatter_for_js()
+  -- Get all files in the current directory
+  local root_files = vim.fn.readdir(vim.fn.getcwd())
+
+  -- Prettier check
+  for _, config in ipairs(prettier_configs) do
+    if vim.tbl_contains(root_files, config) then return { "prettierd" } end
+  end
+
+  -- Biome check
+  for _, config in ipairs(biome_configs) do
+    if vim.tbl_contains(root_files, config) then return { "biome" } end
+  end
+
+  -- ESLint check
+  for _, config in ipairs(eslint_configs) do
+    if vim.tbl_contains(root_files, config) then return { "eslint" } end
+  end
+
+  -- Fallback formatter
+  return { "prettierd" }
+end
+
+local js_formatter = formatter_for_js()
+
+-- This is just for testing that the above method works correclty for finding the file
+-- uncomment and then you can use :TestFormatter as a command to test
+-- vim.api.nvim_create_user_command("TestFormatter", function() print(vim.inspect(formatter_for_js())) end, {})
+
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>lf",
+      function() require("conform").format { async = true, lsp_fallback = true } end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      javascript = js_formatter,
+      typescript = js_formatter,
+      javascriptreact = js_formatter,
+      typescriptreact = js_formatter,
+      json = js_formatter,
+      jsonc = js_formatter,
+      svelte = { "prettierd" },
+      css = { "prettierd" },
+      html = { "prettierd" },
+      yaml = { "prettierd" },
+      markdown = { "prettierd" },
+      graphql = { "prettierd" },
+      lua = { "stylua" },
+      python = { "isort", "black" },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+  },
+}
--- .config/personal_nvim/lua/plugins/lsp.lua
+++ .config/personal_nvim/lua/plugins/lsp.lua
@@ -50,6 +50,10 @@ return {
         }
       end
 
+      -- lint/formatters
+      lspconfig.eslint.setup {}
+      lspconfig.biome.setup {}
+
       -- key bindings
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("LspKeybindings", { clear = true }),
@@ -63,132 +67,11 @@ return {
           vim.keymap.set("n", "<leader>lh", vim.lsp.buf.signature_help, opts)
           vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
           vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format { async = true } end, opts)
+          -- using conform now
+          -- vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format { async = true } end, opts)
           vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float)
         end,
       })
-    end,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-    },
-    opts = {
-      ensure_installed = { "prettierd", "stylua", "biome" },
-      automatic_installation = false,
-      handlers = {
-        prettierd = function(source_name, methods)
-          -- Conditional to only use prettier when a .pretterrc is in root
-          local null_ls = require "null-ls"
-          null_ls.register(null_ls.builtins.formatting.prettierd.with {
-            condition = function(utils)
-              return utils.root_has_file(
-                ".prettierrc",
-                ".prettierrc.js",
-                ".prettierrc.cjs",
-                ".prettierrc.json",
-                ".prettierrc.yaml",
-                ".prettierrc.yml",
-                ".prettierrc.toml",
-                "prettier.config.cjs"
-              )
-            end,
-          })
-        end,
-        eslint = function(source_name, methods)
-          -- Conditional to only use eslint when a .eslint is in root
-          local null_ls = require "null-ls"
-          null_ls.register(require("none-ls.formatting.eslint").with {
-            condition = function(utils)
-              return utils.root_has_file(
-                ".eslintrc",
-                ".eslintrc.json",
-                ".eslintrc.js",
-                ".eslintrc.yml",
-                ".eslintrc.yaml"
-              )
-            end,
-          })
-        end,
-        biome = function(source_name, methods)
-          -- Conditional to only use biome when a biome.json is in root
-          local null_ls = require "null-ls"
-          null_ls.register(null_ls.builtins.formatting.biome.with {
-            condition = function(utils) return utils.root_has_file "biome.json" end,
-          })
-        end,
-      },
-    },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-      local null_ls = require "null-ls"
-      null_ls.setup {
-        sources = {
-          -- Lua
-          null_ls.builtins.formatting.stylua,
-          -- JS
-          null_ls.builtins.formatting.prettierd.with {
-            condition = function(utils)
-              return utils.root_has_file(
-                ".prettierrc",
-                ".prettierrc.js",
-                ".prettierrc.cjs",
-                ".prettierrc.json",
-                ".prettierrc.yaml",
-                ".prettierrc.yml",
-                ".prettierrc.toml",
-                "prettier.config.cjs"
-              )
-            end,
-          },
-          null_ls.builtins.formatting.biome.with {
-            condition = function(utils) return utils.root_has_file "biome.json" end,
-          },
-
-          -- eslint
-          require("none-ls.diagnostics.eslint").with {
-            condition = function(utils)
-              return utils.root_has_file(
-                ".eslintrc",
-                ".eslintrc.json",
-                ".eslintrc.js",
-                ".eslintrc.yml",
-                ".eslintrc.yaml"
-              )
-            end,
-          },
-
-          require("none-ls.formatting.eslint").with {
-            condition = function(utils)
-              return utils.root_has_file(
-                ".eslintrc",
-                ".eslintrc.json",
-                ".eslintrc.js",
-                ".eslintrc.yml",
-                ".eslintrc.yaml"
-              )
-            end,
-          },
-
-          require("none-ls.code_actions.eslint").with {
-            condition = function(utils)
-              return utils.root_has_file(
-                ".eslintrc",
-                ".eslintrc.json",
-                ".eslintrc.js",
-                ".eslintrc.yml",
-                ".eslintrc.yaml"
-              )
-            end,
-          },
-        },
-      }
     end,
   },
 }
