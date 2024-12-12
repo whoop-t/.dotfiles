82b4bba5d96b4dbe28a63f9cf3e108b07dad9d28
whoop-t <tommymallis@gmail.com>
2024-12-11 18:18:09 -0800

    feat: copy file path mapping

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,13 +2,18 @@
 -- Leader key mapping
 vim.g.mapleader = " "
 -- better j and k
-vim.keymap.set({"n", "x"}, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true, desc = "Move cursor down" })
-vim.keymap.set({"n", "x"}, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = "Move cursor up" })
+vim.keymap.set(
+  { "n", "x" },
+  "j",
+  "v:count == 0 ? 'gj' : 'j'",
+  { expr = true, silent = true, desc = "Move cursor down" }
+)
+vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = "Move cursor up" })
 -- Key to clear highlight search
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 -- save and quit mappings
-vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save"})
-vim.keymap.set("n", "<leader>q", "<cmd>confirm q<cr>", { desc = "Quit"})
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
+vim.keymap.set("n", "<leader>q", "<cmd>confirm q<cr>", { desc = "Quit" })
 -- blackhole delete
 vim.keymap.set({ "n", "v" }, "<leader>p", '"_dP')
 -- Center text when moving with C-d and C-u
@@ -16,3 +21,10 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- tmux sessionizer in nvim
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>")
+-- copy current file name
+vim.keymap.set(
+  "n",
+  "<leader>cf",
+  '<cmd>let @+ = fnamemodify(expand("%:p"), ":.")<cr>',
+  { desc = "Copy current file path" }
+)
