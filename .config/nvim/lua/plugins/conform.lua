dca9d8e82e613ca02eee5c50feb5fc155ce46b4f
whoop-t <tommymallis@gmail.com>
2025-01-17 19:08:00 -0800

    fix: add scss to conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -86,6 +86,7 @@ return {
       jsonc = { "prettierd" },
       svelte = { "prettierd" },
       css = { "prettierd" },
+      scss = { "prettierd" },
       html = { "prettierd" },
       yaml = { "prettierd" },
       markdown = { "prettierd" },
