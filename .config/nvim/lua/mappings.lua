64dd15240e82c7bce599832d0099b9d3cf40bc54
whoop-t <tommymallis@gmail.com>
2025-02-25 16:19:01 -0800

    fix: add back diag map, change snacks diag to s

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -30,3 +30,20 @@ vim.keymap.set(
   '<cmd>let @+ = fnamemodify(expand("%:p"), ":.")<cr>',
   { desc = "Copy current file path" }
 )
+-- Show diagnostics with lsp that outputted it
+-- This opens a float with diag info
+vim.keymap.set("n", "<leader>ld", function()
+  vim.diagnostic.open_float(nil, {
+    border = "rounded",
+    -- Customize how each diagnostic is formatted
+    format = function(diagnostic)
+      if diagnostic.source then
+        return string.format("[%s] %s", diagnostic.source, diagnostic.message)
+      else
+        return diagnostic.message
+      end
+    end,
+    prefix = "", -- Remove prefix aka numbered list
+    header = "", -- Remove the title
+  })
+end, { desc = "Show diagnostics with source" })
