56a461a29d3a048a86d33a6fb2e70ace7ba29231
whoop-t <tommymallis@gmail.com>
2025-04-06 11:21:19 -0700

    update debug log snippet for js

--- .config/nvim/lua/snippets/javascript.lua
+++ .config/nvim/lua/snippets/javascript.lua
@@ -2,6 +2,7 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local rep = require("luasnip.extras").rep
 
 return {
   s({
@@ -10,8 +11,12 @@ return {
     dscr = "Debug Console log shortcut",
     autosnippet = true,
   }, {
-    t({ 'console.log("##################");', "console.log(" }),
-    i(1, ""),
-    t({ ");", 'console.log("##################");' }),
+    t('console.log("##### '),
+    rep(1),
+    t(' #####");'),
+    t({ "", "console.log(" }),
+    i(1),
+    t(");"),
+    t({ "", 'console.log("#############");' }),
   }),
 }
