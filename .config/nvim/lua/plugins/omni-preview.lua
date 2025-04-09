73ced855a037b069874aaf48d71fceb92d3ba3f9
whoop-t <tommymallis@gmail.com>
2025-04-09 12:41:02 -0700

    open peek preview in browser

--- .config/nvim/lua/plugins/omni-preview.lua
+++ .config/nvim/lua/plugins/omni-preview.lua
@@ -33,7 +33,9 @@ return {
       event = { "VeryLazy" },
       build = "deno task --quiet build:fast",
       config = function()
-        require("peek").setup()
+        require("peek").setup({
+          app = 'browser',
+        })
         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
       end,
