2ac9a73a0f193f6835ac425437a96f6ed5b4d65d
whoop-t <tommymallis@gmail.com>
2025-04-12 16:44:32 -0700

    fix: add peek specific keybindings for omni conflicts

--- .config/nvim/lua/plugins/omni-preview.lua
+++ .config/nvim/lua/plugins/omni-preview.lua
@@ -39,6 +39,9 @@ return {
         })
         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+
+        vim.keymap.set("n", "<leader>mo", function() require("peek").open() end, { desc = "OmniPreview" })
+        vim.keymap.set("n", "<leader>mc", function() require("peek").close() end, { desc = "OmniPreview" })
       end,
     },
     {
