f800196702c178bd355141a682556ab66e6e5b11
whoop-t <tommymallis@gmail.com>
2025-04-12 12:47:49 -0700

    feat: change to omni-preview for cloak

--- .config/nvim/lua/plugins/omni-preview.lua
+++ .config/nvim/lua/plugins/omni-preview.lua
@@ -1,6 +1,7 @@
 return {
   -- manage previews/opens
   -- will open pdfs/images/etc
+  -- dir = '~/dev/omni-preview.nvim', -- for local dev
   "sylvanfranklin/omni-preview.nvim",
   dependencies = {
     -- CSV
@@ -40,9 +41,44 @@ return {
         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
       end,
     },
+    {
+      "laytan/cloak.nvim",
+      lazy = false,
+      config = function()
+        require("cloak").setup {
+          enabled = true,
+          cloak_character = "*",
+          -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
+          highlight_group = "Comment",
+          -- Applies the length of the replacement characters for all matched
+          -- patterns, defaults to the length of the matched pattern.
+          cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
+          -- Whether it should try every pattern to find the best fit or stop after the first.
+          try_all_patterns = true,
+          patterns = {
+            {
+              -- Match any file starting with '.env'.
+              -- This can be a table to match multiple file patterns.
+              file_pattern = { ".env*", ".npmrc*", "local.cjs*", "credentials*" },
+              -- Match an equals sign and any character after it.
+              -- This can also be a table of patterns to cloak,
+              -- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
+              cloak_pattern = { "=.+", ":.+" },
+              -- A function, table or strcng to generate the replacement.
+              -- The actual replacement will contain the 'cloak_character'
+              -- where it doesn't cover the original text.
+              -- If left empty the legacy behavior of keeping the first character is retained.
+              replace = nil,
+            },
+          },
+        }
+      end,
+    },
   },
   opts = {},
   keys = {
     { "<leader>to", "<cmd>:OmniPreview toggle<CR>", desc = "OmniPreview" },
+    { "<leader>te", "<cmd>:OmniPreview start<CR>",  desc = "OmniPreview" },
+    { "<leader>td", "<cmd>:OmniPreview stop<CR>",   desc = "OmniPreview" },
   }
 }
