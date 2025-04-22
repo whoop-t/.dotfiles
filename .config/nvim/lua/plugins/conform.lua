22835ac8b427f92a4483bf86f0b0e3117bb1b5c3
whoop-t <tommymallis@gmail.com>
2025-04-22 07:28:32 -0700

    feat: format on save

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -98,6 +98,10 @@ return {
     default_format_opts = {
       lsp_format = "fallback",
     },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
   },
 }
-
