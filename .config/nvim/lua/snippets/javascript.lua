bf580aeb9d23a8295d4c356597f30a0c63f2370e
whoop-t <tommymallis@gmail.com>
2025-05-04 10:30:24 -0700

    feat: add js debug snippet yank

--- .config/nvim/lua/snippets/javascript.lua
+++ .config/nvim/lua/snippets/javascript.lua
@@ -2,6 +2,7 @@ local ls = require "luasnip"
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
 local rep = require("luasnip.extras").rep
 
 return {
@@ -16,6 +17,21 @@ return {
     t ' #####");',
     t { "", "console.log(" },
     i(1),
+    t ");",
+    t { "", 'console.log("#############");' },
+  }),
+
+  s({
+    trig = "dcly",
+    name = "debug Console Log from yank",
+    dscr = "debug Console log from yank shortcut",
+    autosnippet = true,
+  }, {
+    t 'console.log("##### ',
+    f(function(_, snip) return snip.env.CLIPBOARD[1] or {} end, {}),
+    t ' #####");',
+    t { "", "console.log(" },
+    f(function(_, snip) return snip.env.CLIPBOARD[1] or {} end, {}),
     t ");",
     t { "", 'console.log("#############");' },
   }),
