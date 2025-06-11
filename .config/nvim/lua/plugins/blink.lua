4a36c820707a3c84bf68ee42b7781ff2c2c050ad
whoop-t <tommymallis@gmail.com>
2025-06-11 11:04:13 -0700

    feat: add copilot

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -63,7 +63,7 @@ return {
     version = "v0.*",
 
     opts = {
-      keymap = { preset = "default" },
+      keymap = { preset = "default", ["<Tab>"] = {}, ["<S-Tab>"] = {} }, -- disable tab key
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,43 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot", -- Load on Copilot command usage
+  event = "InsertEnter", -- Load when entering insert mode
+  config = function()
+    require("copilot").setup {
+      suggestion = { enabled = true, auto_trigger = true, debounce = 50 },
+      panel = { enabled = false },
+    }
+  end,
+  keys = {
+    {
+      "<leader>ct",
+      function()
+        require("copilot.suggestion").toggle_auto_trigger()
+        local state = vim.b.copilot_suggestion_auto_trigger
+        if state then
+          vim.notify "Copilot Auto-trigger: ON"
+        else
+          vim.notify "Copilot Auto-trigger: OFF"
+        end
+      end,
+      mode = "n",
+      desc = "Copilot Toggle",
+    },
+    {
+      "<leader>cd",
+      function()
+        if require("copilot.suggestion").is_visible() then require("copilot.suggestion").dismiss() end
+      end,
+      mode = "n",
+      desc = "Copilot Accept",
+    },
+    {
+      "<Tab>",
+      function()
+        if require("copilot.suggestion").is_visible() then require("copilot.suggestion").accept() end
+      end,
+      mode = "i",
+      desc = "Copilot Accept",
+    },
+  },
+}
