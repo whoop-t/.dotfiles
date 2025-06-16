7fb35edb930ddc1624a91df2b3912305b19d34fa
whoop-t <tommymallis@gmail.com>
2025-06-16 13:10:30 -0700

    feat: add toggle to vim illuminate

--- .config/nvim/lua/plugins/vim-illuminate.lua
+++ .config/nvim/lua/plugins/vim-illuminate.lua
@@ -1,5 +1,14 @@
 -- word highlight with other symbols
 return {
   "RRethy/vim-illuminate",
+  lazy = false, -- need as the keys map make this only load on key press toggle
   config = function() require("illuminate").configure() end,
+  keys = {
+    -- toggle word highlight
+    {
+      "<leader>it",
+      function() require("illuminate").toggle() end,
+      desc = "Toggle word highlight",
+    },
+  },
 }
