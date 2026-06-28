9dfb7e0dea5663e4eae3434faa381a3d937d1a3b
whoop-t <tommymallis@gmail.com>
2026-06-28 11:27:06 -0700

    format

--- .config/astro_nvim/lua/plugins/tokyonight.lua
+++ .config/astro_nvim/lua/plugins/tokyonight.lua
@@ -3,11 +3,11 @@ return {
   priority = 1000,
   plugins = { markdown = true },
   opts = {
-    style = 'night',
-		styles = {
-			sidebars = "transparent",
-			floats = "transparent",
-		},
-    transparent = true
-  }
+    style = "night",
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+    transparent = true,
+  },
 }
